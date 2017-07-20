037a30ecffb3a52b580a8079e956079240e23558
Winston Ku <wdku5791@gmail.com>
2017-07-20 14:28:36 -0700

    a bit more css styling on thumbnails

--- client/src/components/doctorDetails.jsx
+++ client/src/components/doctorDetails.jsx
@@ -4,12 +4,14 @@ const doctorDetail = ({ doctor, selector }) => {
   return (
     <div className='detail' onClick={(e) => {selector(null)}}>
       <img src={ doctor.picture }></img>
-      <div>{`Name: ${doctor.firstName} ${doctor.lastName}`}</div>
-      <div>{`Location: ${doctor.area}`}</div>
-      <div>{`Specialty: ${doctor.specialty}`}</div>
-      <div>{`Rating: ${doctor.rating}`}</div>
-      <div>{`Coverage type: ${doctor.insurance}`}</div>
-      <div>{`Details: ${doctor.details}`}</div>
+      <div className="options">
+        <div>{`Name: ${doctor.firstName} ${doctor.lastName}`}</div>
+        <div>{`Location: ${doctor.area}`}</div>
+        <div>{`Specialty: ${doctor.specialty}`}</div>
+        <div>{`Rating: ${doctor.rating}`}</div>
+        <div>{`Coverage type: ${doctor.insurance}`}</div>
+        <div>{`Details: ${doctor.details}`}</div>
+      </div>
     </div>
   )
 }
